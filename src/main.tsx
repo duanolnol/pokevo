d6876042df972d28cc58e28b1f838fd95d441d75
Budiyono <budiyono@telkomsel.co.id>
2024-04-23 13:04:05 +0700

    chore: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,20 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Pokemon from "./presenter/pages/Pokemon";
+import * as Sentry from "@sentry/react";
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
 
 const queryclient = new QueryClient();
 
-const router = createBrowserRouter([
+const sentryCreateBrowserRouter =
+  Sentry.wrapCreateBrowserRouter(createBrowserRouter);
+
+const router = sentryCreateBrowserRouter([
   {
     path: "/",
     element: <App />,
@@ -18,6 +28,31 @@ const router = createBrowserRouter([
     element: <Pokemon />,
   },
 ]);
+
+Sentry.init({
+  dsn: "https://e3db1b3b7407756efb33c978fa8803f6@o4507134777556992.ingest.us.sentry.io/4507134784241664",
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "duanolnol",
+        project: "pokevo",
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        telemetry: false
+      }),
+    ],
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: true, // Source map generation must be turned on
+    },
+  };
 });
